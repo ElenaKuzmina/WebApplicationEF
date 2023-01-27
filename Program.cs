bff2e93e7ade5b6b8d6a6dff36cf99992c7358bc
admin <admin@DESKTOP-8HAIHSV>
2023-01-27 10:02:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplicationEF.Classes;
+
+var builder = WebApplication.CreateBuilder();
+
+// получаем строку подключения из файла конфигурации
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// добавляем контекст ApplicationContext в качестве сервиса в приложение
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
+
+var app = builder.Build();
+// получение данных
+app.MapGet("/", (ApplicationContext db) => db.Users.ToList());
+app.Run();
